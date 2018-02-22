10a5238528e03dbd9704727000309f900a4aecca
zhenpanda88 <jzd@case.edu>
2018-02-21 22:50:19 -0500

    Updated relay controller and publisher with compiliable code. Still need to correctly import libraries.

--- src/usb_relay_controller.cpp
+++ src/usb_relay_controller.cpp
@@ -1,17 +1,22 @@
 #include <ros/ros.h>
 #include <stdlib.h>
-#include <std_msgs/Bool.>
-#include <libusb-1.0/libusb.h>
-#include <usb_relay_device.h>
-#include <usb_relay_lib.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/String.h>
+//#include <libusb-1.0/libusb.h>
+//#include <usb_relay_device.h>
+//#include <usb_relay_lib.h>
 
-std_msgs::Bool state;
+std_msgs::Int16 state;
+std_msgs::String relayId;
 
-void myCallback(const std_msgs::Bool& On_Off_State){
-
-	ROS_INFO("Received RELAY State of (1 is on, 0 is off): ", On_Off_State.data);
+void onOffStateCallback(const std_msgs::Int16& On_Off_State){
+	ROS_INFO("Received RELAY State of (1 is on, 0 is off): %i", On_Off_State.data);
 	state.data = On_Off_State.data;
+}
 
+void relayDeviceIdCallback(const std_msgs::String::ConstPtr& id){
+	ROS_INFO("Received Relay ID of (1, 2, or ALL): %s", id->data.c_str());
+	relayId.data = id->data.c_str();
 }
 
 int main(int argc, char **argv) {
@@ -19,12 +24,20 @@ int main(int argc, char **argv) {
     // when this compiled code is run, ROS will recognize it as a node called "minimal_simulator" 
     ros::NodeHandle n; // node handle 
     //create a Subscriber object and have it subscribe to the topic "force_cmd" 
-    ros::Subscriber my_subscriber_object = n.subscribe("On_Off_State", 1, myCallback);
+    ros::Subscriber onOff_sub = n.subscribe("On_Off_State", 1, onOffStateCallback);
+    ros::Subscriber relayID_sub = n.subscribe("Relay_ID", 1, relayDeviceIdCallback);
     //simulate accelerations and publish the resulting velocity; 
+    ros::Rate naptime(1.0);
     
+
+
     while (ros::ok()) {
-        ROS_INFO("velocity = %f", g_velocity.data);
         
+        //usb_relay_init(); //initializes the usb relay
+
+
+
+
         ros::spinOnce(); //allow data update from callback 
         
         naptime.sleep(); // wait for remainder of specified period; this loop rate is faster than 
--- src/usb_relay_publisher.cpp
+++ src/usb_relay_publisher.cpp
@@ -1,9 +1,10 @@
 #include <ros/ros.h>
 #include <stdlib.h>
-#include <std_msgs/Bool.>
-#include <libusb-1.0/libusb.h>
-#include <usb_relay_device.h>
-#include <usb_relay_lib.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/String.h>
+//#include <libusb-1.0/libusb.h>
+//#include <usb_relay_device.h>
+//#include <usb_relay_lib.h>
 
 
 int main(int argc, char **argv)
@@ -11,20 +12,25 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "usb_relay_publisher");
 	ros::NodeHandle n;
 
-	ros::Publisher relay_on_off = n.advertise<std_msgs::Bool>("On_Off_State", 1);
+	ros::Publisher relay_on_off = n.advertise<std_msgs::Int16>("On_Off_State", 1);
+	ros::Publisher relay_device_id = n.advertise<std_msgs::String>("Relay_ID", 1);
 
-
-	bool OnOff;
-	string relay_device_id;
+	std_msgs::Int16 OnOff;
+	int state;
+	std_msgs::String id;
 
 	std::cout << "Enter relay id (1, 2 or ALL): ";
-	std::cin >> relay_device_id;
+	std::cin >> id.data;
 
 	std::cout << "Enter Relay State (1 is on, 0 is off): ";
-	std::cin >> OnOff;
+	std::cin >> state;
+
+	OnOff.data = state;	
 
 	while(ros::ok())
 	{
+		relay_on_off.publish(OnOff);
+		relay_device_id.publish(id);
 
 		ros::Duration(1).sleep();
 	}
